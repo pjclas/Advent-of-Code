118f5edc27e7b0ebd131f3a88025dd1fe93b70df
pjclas <pjclas@gmail.com>
2023-12-27 21:38:45 -0500

    AoC 2023 updates

--- 2023/day17-2023.php
+++ 2023/day17-2023.php
@@ -1,8 +1,14 @@
 <?php
-const INT_MAX = 2147483647;
+
+class MyHeap extends SplMinHeap
+{
+    public function compare($item1, $item2):int {
+        return ($item1[0]<$item2[0]?1:-1);
+    }
+}
 
 $dirs = [[-1,0],[0,1],[1,0],[0,-1]];
-$leastHeatLoss = INT_MAX;
+
 function readInput()
 {
     global $map, $endNode;
@@ -13,128 +19,20 @@ function readInput()
     $endNode = [count($map)-1, count($map[0])-1];
 }
 
-function findPath1($node, &$cache = array(), $trace="") {
-    global $map, $dirs, $leastHeatLoss, $endNode, $data;
-    $key = $node["dir"].$node["dirCount"].implode(',',$node["pos"]);
-
-    if ($node["pos"] == $endNode) {
-        if ($node["loss"] < $leastHeatLoss) $leastHeatLoss = $node["loss"];
-        print "End node heat loss = $leastHeatLoss\n";
-        return true;
-    }
-
-    // check if we've been here before or if we are out of the map
-    if (array_key_exists($key, $cache)) {
-        if ($cache[$key]["loss"] > $node["loss"]) {
-          //  print "decreasing loss from ".$cache[$key]["loss"]." to ".$node["loss"]."\n";
-            $cache[$key]["loss"] = $node["loss"];
-        }
-        else {
-            return false;
-        }
-    }
-    if ($node["loss"] >= $leastHeatLoss) return false;
-
-   // $data.="$trace$key - loss=".$node["loss"]."\n";
-    $cache[$key] = $node;
-    foreach (range(-1,1) as $dir) {
-        $newNode = $node;
-        if ($dir == 0) {
-            // we are going straight
-            if ($newNode["dirCount"]>0) {
-                $newNode["dirCount"]--;
-            } else continue;
-        } else {
-            // we are turning, figure out the new direction
-            $newNode["dirCount"] = 2;
-            $newNode["dir"]+=$dir+4;
-            $newNode["dir"]%=4;
-        }
-        $newNode["pos"] = [$newNode["pos"][0]+$dirs[$newNode["dir"]][0],$newNode["pos"][1]+$dirs[$newNode["dir"]][1]];
-        if (isset($map[$newNode["pos"][0]][$newNode["pos"][1]])) {
-            $newNode["loss"]+=$map[$newNode["pos"][0]][$newNode["pos"][1]];
-            findPath1($newNode, $cache, " >" . $trace);
-        }
-    }
-
-    return false;
-}
-
-function findPath2($node, &$cache = array(), $trace="") {
-    global $map, $dirs, $leastHeatLoss, $endNode, $data;
-    $key = $node["dir"].$node["dirCount"].implode(',',$node["pos"]);
-
-    // check if we can stop at or turn before the end
-    if ($node["dirCount"] < 4) {
-        $moves = 4-$node["dirCount"];
-        if (($node["dir"] == 0 && $moves > $node["pos"][0]) ||
-            ($node["dir"] == 1 && $moves > (count($map[0])-$node["pos"][1])-1) ||
-            ($node["dir"] == 2 && $moves > (count($map)-$node["pos"][0])-1) ||
-            ($node["dir"] == 3 && $moves > $node["pos"][1])) {
-            //  print implode(",",$node["pos"])." can't go dir ".$node["dir"].", required $moves ".$node["dirCount"]."\n";
-            return false;
-        }
-    }
-
-    if ($node["pos"] == $endNode) {
-        if ($node["loss"] < $leastHeatLoss) $leastHeatLoss = $node["loss"];
-        print "End node heat loss = $leastHeatLoss\n";
-        print $node["path"]."\n";
-        return true;
-    }
-
-    // check if we've been here before or if we are out of the map
-    if (array_key_exists($key, $cache)) {
-        if ($cache[$key]["loss"] > $node["loss"]) {
-            //  print "decreasing loss from ".$cache[$key]["loss"]." to ".$node["loss"]."\n";
-            $cache[$key]["loss"] = $node["loss"];
-        }
-        else {
-            return false;
-        }
-    }
-    if ($node["loss"] >= $leastHeatLoss) return false;
-
-    //$data.="$trace$key - loss=".$node["loss"]."\n";
-    $cache[$key] = $node;
-    foreach (range(-1,1) as $dir) {
-        $newNode = $node;
-        if ($dir == 0) {
-            // we are going straight
-            if ($newNode["dirCount"]<10) {
-                $newNode["dirCount"]++;
-            } else continue;
-        } else {
-            if ($newNode["dirCount"] < 4 && $newNode["dirCount"] != 0) continue;
-            // we are turning, figure out the new direction
-            $newNode["dirCount"] = 1;
-            $newNode["dir"]+=$dir+4;
-            $newNode["dir"]%=4;
-        }
-        $newNode["pos"] = [$newNode["pos"][0]+$dirs[$newNode["dir"]][0],$newNode["pos"][1]+$dirs[$newNode["dir"]][1]];
-        if (isset($map[$newNode["pos"][0]][$newNode["pos"][1]])) {
-            $newNode["loss"]+=$map[$newNode["pos"][0]][$newNode["pos"][1]];
-            $newNode["path"].=" ".$newNode["pos"][0].",".$newNode["pos"][1];
-            findPath2($newNode, $cache, " >" . $trace);
-        }
-    }
-
-    return false;
-}
-
-function findPath3($map, $start, $end) {
+function findPath($map, $start, $end, $minStepsBeforeTurn=0, $maxStepsForward=3) {
     global $dirs;
 
     $cache = array();
-    $nodes = [[0, $start[0], $start[1], 1, 0]];
-    while (!empty($nodes)) {
-        list($heat, $r, $c, $dir, $count) = array_shift($nodes);
+    $nodes = new MyHeap();
+    $nodes->insert([0, $start, 1, 0]);
+    while (!$nodes->isEmpty()) {
+        list($heat, $pos, $dir, $count) = $nodes->extract();
 
-        if ([$r,$c] == $end) {
+        if ($pos == $end) {
             return $heat;
         } else {
             // check if we've been here before or if we are back to the start
-            $key = $dir.":".$count.":".$r.",".$c;
+            $key = $dir.":".$count.":".$pos[0].",".$pos[1];
             if (array_key_exists($key, $cache)) {
                 continue;
             }
@@ -145,127 +43,57 @@ function findPath3($map, $start, $end) {
                 if ($d == 0) {
                     $nextDir = $dir;
                     // we are going straight
-                    if ($count<10) {
+                    if ($count<$maxStepsForward) {
                         $nextCount = $count+1;
                     } else continue;
                 } else {
-                    if ($count < 4 && $count != 0) continue;
+                    if ($count < $minStepsBeforeTurn && $count != 0) continue;
                     // we are turning, figure out the new direction
                     $nextCount = 1;
                     $nextDir=($dir+$d+4)%4;
                 }
-                $nextRow = $r+$dirs[$nextDir][0];
-                $nextCol = $c+$dirs[$nextDir][1];
-                if ([$nextRow, $nextCol] == $start) continue;
+                $nextPos = [$pos[0]+$dirs[$nextDir][0], $pos[1]+$dirs[$nextDir][1]];
+                if ($nextPos == $start) continue;
 
                 // check if we are out of the map
-                if (isset($map[$nextRow][$nextCol])) {
+                if (isset($map[$nextPos[0]][$nextPos[1]])) {
                     // check if we can stop at or turn before the end
-/*                    if ($nextCount < 4) {
-                        $moves = 4-$nextCount;
-                        if (($nextDir == 0 && $moves > $nextRow) ||
-                            ($nextDir == 1 && $moves > count($map[0])-$nextCol-1) ||
-                            ($nextDir == 2 && $moves > count($map)-$nextRow-1) ||
-                            ($nextDir == 3 && $moves > $nextCol)) {
+                    if ($nextCount < $minStepsBeforeTurn) {
+                        $moves = $minStepsBeforeTurn-$nextCount;
+                        if (($nextDir == 0 && $moves > $nextPos[0]) ||
+                            ($nextDir == 1 && $moves > count($map[0])-$nextPos[1]-1) ||
+                            ($nextDir == 2 && $moves > count($map)-$nextPos[0]-1) ||
+                            ($nextDir == 3 && $moves > $nextPos[1])) {
                             continue;
                         }
-                    }*/
-                    $nextHeat=$heat+$map[$nextRow][$nextCol];
-                    $nodes[] = [$nextHeat, $nextRow, $nextCol, $nextDir, $nextCount];
-                    array_multisort(array_column($nodes, 0), SORT_ASC, $nodes);
+                    }
+                    $nextHeat=$heat+$map[$nextPos[0]][$nextPos[1]];
+                    $nodes->insert([$nextHeat, $nextPos, $nextDir, $nextCount]);
                 }
             }
         }
     }
-}
-function findPath4($map, $start, $end) {
-    global $dirs;
-
-    $height = count($map);
-    $width = count($map[0]);
-    $cache = array();
-    $nodes = [[0, $start[0], $start[1], 0, 0, 0]];
-    while ($nodes) {
-        list($heat, $r, $c, $dr, $dc, $count) = array_shift($nodes);
-
-        // check if we've been here before or if we are back to the start
-        $key = $r . "," . $c . ":" . $dr . "," . $dc . ":" . $count;
-        if (isset($cache[$key])) continue;
-        if ($r == $height-1 && $c == $width-1) {
-            return $heat;
-        }
-        // add the node to our cache
-        $cache[$key] = 1;
-
-        foreach ($dirs as list($next_dr, $next_dc)) {
-            $nextR = $r + $next_dr;
-            $nextC = $c + $next_dc;
-            if ($nextR < 0 || $nextC < 0 || $nextR >=$height || $nextC >= $width) continue;
-            if ($next_dr==-$dr && $next_dc==-$dc) continue;
-            if ($next_dr==$dr && $next_dc==$dc) {
-                if ($count < 10)
-                    $nodes[] = [$heat+$map[$nextR][$nextC], $nextR, $nextC, $next_dr, $next_dc, $count+1];
-                else continue;
-            } elseif ($count>=4 || ($dr==0 && $dc==0)) {
-                $nodes[] = [$heat+$map[$nextR][$nextC], $nextR, $nextC, $next_dr, $next_dc, 1];
-            }
-            array_multisort(array_column($nodes, 0), SORT_ASC, $nodes);
-        }
-    }
+    return 0;
 }
 
 function part1() {
-    global $leastHeatLoss, $map;
-    $node["dir"] = 1;
-    $node["dirCount"] = 3;
-    $node["pos"] = [0,0];
-    $node["loss"] = 0;
-    $col=1;
-    $leastHeatLoss = 0;
-    $end = [count($map)-1, count($map[0])-1];
-    foreach ($map as $row=>$data) {
-        if ([$row,$col]==$end) break;
-        $leastHeatLoss+=$map[$row][$col];
-
-        $leastHeatLoss+=$map[$row+1][$col];
-        if ([$row+1,$col]==$end) break;
-        $col++;
-    }
-    findPath1($node);
-
-    print "Part 1: The least heat loss is $leastHeatLoss\n";
+    global $map;
+    $heatLoss = findPath($map, [0,0], [count($map)-1, count($map[0])-1]);
+    print "Part 1: The least heat loss is $heatLoss\n";
 }
 
 function part2() {
-    global $leastHeatLoss, $map, $data;
-/*    $data="";
-    $leastHeatLoss = 0;
-    $numLoops = intdiv((count($map)-1),4)-1;
-    $remain = (count($map)-1)%4+4;
-    $row=$col=0;
-    foreach (range(0,$numLoops-1) as $c) {
-        for ($j = 0; $j < 4; $j++) {
-            $col++;
-            $leastHeatLoss += $map[$row][$col];
-        }
-        for ($i=0; $i < 4; $i++) {
-            $row++;
-            $leastHeatLoss += $map[$row][$col];
-        }
-    }
-    for ($j = 0; $j < $remain; $j++) {
-        $col++;
-        $leastHeatLoss += $map[$row][$col];
-    }
-    for ($i=0; $i < $remain; $i++) {
-        $row++;
-        $leastHeatLoss += $map[$row][$col];
-    }*/
-    $heatLoss = findPath4($map, [0,0], [count($map)-1, count($map[0])-1]);
-    //file_put_contents("debug.txt", $data);
-
+    global $map;
+    $heatLoss = findPath($map, [0,0], [count($map)-1, count($map[0])-1], 4, 10);
     print "Part 2: The least heat loss is $heatLoss\n";
 }
+
 readInput();
-//part1();
+$start = microtime(true);
+part1();
+$start2 = microtime(true);
+$time_elapsed_secs =  $start2 - $start;
+echo "Time: $time_elapsed_secs\n";
 part2();
+$time_elapsed_secs = microtime(true) - $start2;
+echo "Time: $time_elapsed_secs\n";
